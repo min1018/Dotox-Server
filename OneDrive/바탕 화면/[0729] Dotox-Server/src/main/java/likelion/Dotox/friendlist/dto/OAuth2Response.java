7e0d3f43090e93e979633c36870de9cfb5183ece
장다윤 <da5457@naver.com>
2024-07-30 17:19:50 +0900

    fix : 토큰반환

---
+++ OneDrive/바탕 화면/[0729] Dotox-Server/src/main/java/likelion/Dotox/friendlist/dto/OAuth2Response.java
@@ -0,0 +1,23 @@
+package likelion.Dotox.friendlist.dto;
+
+public interface OAuth2Response {
+
+    //제공자 (Ex. naver, google, ...)
+    String getProvider();
+
+    //제공자에서 발급해주는 아이디(번호)
+    String getProviderId();
+
+    //이메일
+    String getEmail();
+
+    //사용자 실명 (설정한 이름)
+    String getName();
+//
+//    String getGender();
+//
+//    //사용자 나이 (연령대)
+//    String getAge();
+
+
+}
---
+++ OneDrive/바탕 화면/[0729] Dotox-Server/src/test/java/likelion/Dotox/Likelion6ApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.Dotox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

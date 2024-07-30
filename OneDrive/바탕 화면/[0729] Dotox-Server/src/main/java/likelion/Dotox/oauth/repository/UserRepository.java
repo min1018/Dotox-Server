86c9bf49e99746e7d3558cce9e5d5620447482de
min1018 <102647148+min1018@users.noreply.github.com>
2024-07-30 18:57:51 +0900

    fix: 파일 구조 수정

--- OneDrive/바탕 화면/[0729] Dotox-Server/src/main/java/likelion/Dotox/oauth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package likelion.Dotox.oauth.repository;
-
-import likelion.Dotox.oauth.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-    UserEntity findByUsername(String username);
-}

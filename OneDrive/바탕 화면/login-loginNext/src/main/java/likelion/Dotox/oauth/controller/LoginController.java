afcad589b95a31838148a95fe9e972917b2dc8a5
장다윤 <da5457@naver.com>
2024-07-30 19:11:00 +0900

    fix : 토큰반환

---
+++ OneDrive/바탕 화면/login-loginNext/src/main/java/likelion/Dotox/oauth/controller/LoginController.java
@@ -0,0 +1,15 @@
+package likelion.Dotox.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage(){
+
+        return "login";
+    }
+}
